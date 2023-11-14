8f9fe761cd97e0619ac094437049e8ef0ce86bf8
anasalhakim1 <eng.anasalhakim@gmail.com>
2023-11-14 10:52:42 +0200

    add api to url

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('osint.urls')),
+    path('api/', include('osint.urls')),
 ]
 
 if settings.DEBUG:
